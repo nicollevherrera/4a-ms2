1d295af5dbaeabb9394742dbf6d554c96be410c1
maoforero <maforeror@gmail.com>
2021-11-26 09:03:02 -0500

    controller event

--- src/main/java/controllers/EventController.java
+++ src/main/java/controllers/EventController.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import com.grupo5.output_ms.models.Event;
 import com.grupo5.output_ms.repositories.EventRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EventController {
@@ -11,5 +15,18 @@ public class EventController {
         this.eventRepository = eventRepository;
     }
 
+    @GetMapping("/evento/id")
+    Optional<Event> getIdevent(@PathVariable Integer id){
+        return eventRepository.findById(id);
+    }
 
+    @GetMapping ("/eventos")
+    List<Event> listEvent(){
+        return eventRepository.findAll();
+    }
+
+    @PostMapping("/evento")
+    Event newEvent(@RequestBody Event event){
+        return eventRepository.save(event);
+    }
 }
